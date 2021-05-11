8af51c32199433f1f2ae6be798596dec437e06f5
Julian Tran <juliantran@julians-macbook-air.home>
2021-05-11 20:24:25 +0200

    timeout on success

--- src/containers/Success.js
+++ src/containers/Success.js
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom";
 
 const Success = () => {
+  setTimeout(function () {
+    window.location.replace("/");
+  }, 5000);
   return (
     <div className="offer-body">
       <div className="success-box">
         <div className="success">
           <h1>Votre paiement a été accepté !</h1>
+          <p>
+            Vous allez être automatiquement redirigé vers la page d'acceuil dans
+            5 secondes...
+          </p>
 
           <Link to="/">
             <button>Retour à la page d'accueil</button>
